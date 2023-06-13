be9a62580a2f89d42a6bfcd6a21a4afb4841367d
ademusta <sducivilengineers@gmail.com>
2023-06-13 16:14:09 +0300

    pizza order app
    pizza order app

---
+++ pizza_order_app.py
@@ -0,0 +1,41 @@
+# This is a sample Python script.
+print("Welcome to Python Pizza Deliveries!")
+
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+if size == "S":
+    bill += 15
+
+if size == "M":
+    bill += 20
+
+if size == "L":
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+    print(f"your final  bill is :{bill}")
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(ademusta):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
